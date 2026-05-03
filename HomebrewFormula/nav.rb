f76ee0f08488a260061e0782a2a40a50abc0bd37
TheGentleTurtle <TheGentleTurtle@users.noreply.github.com>
2026-05-03 00:53:07 -0400

    Bump formula to v1.1.6

--- HomebrewFormula/nav.rb
+++ HomebrewFormula/nav.rb
@@ -1,8 +1,8 @@
 class Nav < Formula
   desc "Tiny terminal file navigator with vim keys"
   homepage "https://github.com/TheGentleTurtle/nav"
-  url "https://github.com/TheGentleTurtle/nav/archive/refs/tags/v1.1.5.tar.gz"
-  sha256 "783f1f5972868a8a818a4afcb4b43e7d41c315e28a901c4d9626e5a9ea79b8b4"
+  url "https://github.com/TheGentleTurtle/nav/archive/refs/tags/v1.1.6.tar.gz"
+  sha256 "4b35935e20436c5944e6ce6788dacd4840ea041d7863485c78f61472d5d600ce"
   license "CC-BY-NC-4.0"
 
   depends_on "go" => :build
